6a47fa4d169c6319d64cff70e0b7ba501ae51eb2
Marcos <marcossbarbe@gmail.com>
2022-04-22 14:57:20 -0300

    desafio3

--- desafio3/servidor.js
+++ desafio3/servidor.js
@@ -1,10 +1,17 @@
 const http = require('http')
+const fs = require('fs');
 const express = require('express')
+const Contenedor = require("../desafio2/contenedor");
+const res = require('express/lib/response');
+const req = require('express/lib/request');
 
-const app = express()
-const PORT = 8080
-app.listen(PORT)
+const app = express();
+const PORT = 8080;
+app.listen(PORT);
 console.log(`Servidor HTTP en puerto ${PORT}`)
+
+const products = new Contenedor('./desafio2/productos.txt');
+products.init();
 
 app.get('/', (req, res) => {
     res.send("Holis pagina principal")
@@ -14,4 +21,10 @@ app.get('/server', (req, res) => {
     res.send(`Servidor HTTP en puerto ${PORT}`)
 })
 
-http.Server.on("error", error => console.log(`Server error ${error}`))
+app.get('/productos',(req, res) => {
+    res.send(products.getAll());
+}); //Array con todos los Productos
+
+app.get('/productoRandom', (req, res) => {
+    res.send(products.getById(Math.floor(Math.random()* (products.countID) + 1)))
+}) //Producto al azar
